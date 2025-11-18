7903eb3ec7eea3e0107f395916728bab23056710
Arseny Kenunen <arsenykenunen@gmail.com>
2025-11-18 21:55:16 +0300

    moving average smoothing added

--- plots.py
+++ plots.py
@@ -4,7 +4,7 @@ from scipy.ndimage import gaussian_filter1d
 
 fig, ax = plt.subplots(figsize=(8, 6))
 sigma = 10
-
+window_size = 15
 with open("measure_before.csv") as before:
     x_before, y_before = list(), list()
     for line in before.readlines():
@@ -15,11 +15,11 @@ with open("measure_before.csv") as before:
     y_before = (np.array(y_before, dtype='float64') - .15) / .0118
 
     y_filtered_before = y_before - gaussian_filter1d(y_before, sigma)
-
+    y_smooth_before = np.convolve(y_before, np.ones(window_size)/window_size, mode='same')
 #ax.scatter(x_before, y_filtered_before, s=1)
 #ax.scatter(x_before, np.convolve(y_before, np.ones(3000)/3000, mode='valid'))
 #ax.scatter(x_before[13000:16000], y_before[13000:16000], s=1)
-ax.scatter(x_before, y_filtered_before, s=1)
+ax.scatter(x_before, y_smooth_before, s=1)
 
 with open("measure_after.csv") as after:
     x_after, y_after = list(), list()
@@ -31,7 +31,7 @@ with open("measure_after.csv") as after:
     y_after = (np.array(y_after, dtype='float64') - .15) / .0118
 
     y_filtered_after = y_after - gaussian_filter1d(y_after, sigma)
-ax.scatter(x_after, y_filtered_after, s=1)
+#ax.scatter(x_after, y_filtered_after, s=1)
 ax.grid(True, alpha=.3)
 
 plt.show()
