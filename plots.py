1b76f2756929a0b95aa5bf171488b0ca33dad426
||GriB29|| <bekhtin.gm@phystech.edu>
2025-11-21 22:59:39 +0300

    plots update: graphs for pressure and pulses

--- plots.py
+++ plots.py
@@ -1,38 +1,80 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.ndimage import gaussian_filter1d
 
-fig, ax = plt.subplots(figsize=(8, 6))
-sigma = 10
+
+b = 0.14963690399777596
+k = 0.011793942035391742
+
 window_size = 15
+
+
+def show_plot():
+    plt.grid(True, alpha=.3)
+    plt.xlabel("time, s")
+    plt.show()
+    plt.close()
+    plt.figure(figsize=(11, 8))
+
+
+def cut_above_threshold(active_array, copying_array, threshold):
+    active_return, copying_return = list(), list()
+    for i in range(len(active_array)):
+        if active_array[i] >= threshold:
+            active_return.append(active_array[i])
+            copying_return.append(copying_array[i])
+    return active_return, copying_return
+
+
+plt.figure(figsize=(11, 8))
+
+x, y = list(), list()
 with open("measure_before.csv") as before:
-    x_before, y_before = list(), list()
     for line in before.readlines():
         line = line.strip().split(',')
-        x_before.append(line[0])
-        y_before.append(line[1])
-    x_before = np.array(x_before, dtype='float64')
-    y_before = (np.array(y_before, dtype='float64') - .15) / .0118
+        x.append(line[0])
+        y.append(line[1])
+x = np.array(x, dtype='float64')
+y = (np.array(y, dtype='float64') - b) / k
 
-    y_filtered_before = y_before - gaussian_filter1d(y_before, sigma)
-    y_smooth_before = np.convolve(y_before, np.ones(window_size)/window_size, mode='same')
-#ax.scatter(x_before, y_filtered_before, s=1)
-#ax.scatter(x_before, np.convolve(y_before, np.ones(3000)/3000, mode='valid'))
-#ax.scatter(x_before[13000:16000], y_before[13000:16000], s=1)
-ax.scatter(x_before, y_smooth_before, s=1)
+trend_poly = np.poly1d(np.polyfit(x, y, 5))
+y_trend = trend_poly(x)
+y_smooth = np.convolve(y, np.ones(window_size) / window_size, mode='same')
 
+plt.scatter(x, y_smooth, s=1)
+plt.ylabel("blood pressure, mmHg")
+plt.title("Blood pressure over time before physical activity")
+show_plot()
+
+y_cutted, x_cutted = cut_above_threshold(y_smooth - y_trend, x, -2)
+plt.scatter(x_cutted, y_cutted, s=1)
+plt.ylabel("delta pressure, mmHg")
+plt.title("Blood pressure over time before physical activity: pulses")
+show_plot()
+
+
+x, y = list(), list()
 with open("measure_after.csv") as after:
-    x_after, y_after = list(), list()
     for line in after.readlines():
         line = line.strip().split(',')
-        x_after.append(line[0])
-        y_after.append(line[1])
-    x_after = np.array(x_after, dtype='float64')
-    y_after = (np.array(y_after, dtype='float64') - .15) / .0118
+        x.append(line[0])
+        y.append(line[1])
+x = np.array(x, dtype='float64')
+y = (np.array(y, dtype='float64') - b) / k
 
-    y_filtered_after = y_after - gaussian_filter1d(y_after, sigma)
-#ax.scatter(x_after, y_filtered_after, s=1)
-ax.grid(True, alpha=.3)
+trend_poly = np.poly1d(np.polyfit(x, y, 5))
+y_trend = trend_poly(x)
+y_smooth = np.convolve(y, np.ones(window_size)/window_size, mode='same')
 
-plt.show()
-# x=(y-0.15)/0.0118
+plt.scatter(x, y_smooth, s=1)
+plt.ylabel("blood pressure, mmHg")
+plt.title("Blood pressure over time after physical activity")
+show_plot()
+
+y_cutted, x_cutted = cut_above_threshold(y_smooth - y_trend, x, -2)
+plt.scatter(x_cutted, y_cutted, s=1)
+plt.ylabel("delta pressure, mmHg")
+plt.title("Blood pressure over time after physical activity: pulses")
+show_plot()
+
+
+plt.close()
